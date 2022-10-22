69c448b0c33e9598f0387147ae333eb6ce74c094
jlam303 <jplam904@gmail.com>
2022-10-21 18:17:43 -0700

    slideshow stuff

--- gu.js
+++ gu.js
@@ -1,4 +1,17 @@
 var img = document.getElementById("img");
+var images = [], x=0;
+images[0] = "shrimp.png";
+images[1] = "escargots.png";
+images[2] = "beef.png";
+images[3] = "fish.png";
+images[4] = "choco.png";
+setInterval(slideShow, 3000);
 function slideShow(){
     img.src = images[x];
+    if(x == 4){
+        x=0;
+    }
+    else{
+        x++;
+    }
 }
