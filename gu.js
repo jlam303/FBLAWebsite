e97d88c39e2f51f6de58ed8c65cff5347c5284b4
jlam303 <jplam904@gmail.com>
2022-12-16 17:51:05 -0700

    Fixed Slideshow

--- gu.js
+++ gu.js
@@ -1,17 +1,26 @@
 var img = document.getElementById("img");
-var images = [], x=0;
-images[0] = "shrimp.png";
-images[1] = "escargots.png";
-images[2] = "beef.png";
-images[3] = "fish.png";
-images[4] = "choco.png";
-setInterval(slideShow, 3000);
-function slideShow(){
+let imgPreview1 = document.getElementById("preview1");
+let imgPreview2 = document.getElementById("preview2");
+let imgPreview3 = document.getElementById("preview3");
+var images = ["shrimp.png","escargots.png","beef.png","fish.png","choco.png"], x=0;
+
+function slideShowFor(){
+    if(x == images.length-1) x=0;
+    else x++;
     img.src = images[x];
-    if(x == 4){
-        x=0;
-    }
-    else{
-        x++;
-    }
+    imgPreview2.src = images[x];
+    if(x != 0) imgPreview1.src = images[x-1];
+    else imgPreview1.src = images[images.length-1]
+    if(x != images.length-1) imgPreview3.src = images[x+1];
+    else imgPreview3.src = images[0]
+}
+function slideShowBack(){
+    if(x == 0) x=images.length-1;
+    else x--;
+    img.src = images[x];
+    imgPreview2.src = images[x];
+    if(x != 0) imgPreview1.src = images[x-1];
+    else imgPreview1.src = images[images.length-1]
+    if(x != images.length-1) imgPreview3.src = images[x+1];
+    else imgPreview3.src = images[0]
 }
