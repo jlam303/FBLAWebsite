2e534cc65e9f508d37f8111e469337e118c1fca9
mcarri355 <carrillomatisa@gmail.com>
2022-12-30 21:30:33 -0700

    timeline

--- aboutUs.js
+++ aboutUs.js
@@ -1,22 +1,21 @@
 let img = document.getElementById("timelinePic")
 let desc = document.getElementById("timelineDesc").innerHTML
-let pictureChange = ["bestOfPhoenix.png","g.png"]
-let description = ["hello","hi","no","nope"]
+let pictureChange = ["firstPlace.jpg","ratatouille.jpg"]
+let descriptionChange = ["hello","hi"]
 let clicks = 0;
 
 img.src = pictureChange[clicks]
-desc = description[clicks]
+desc = descriptionChange[clicks]
 function next(){
 clicks++;
 if(clicks >= 2) clicks = 0;
 img.src = pictureChange[clicks];
-desc = description[clicks];
+desc = descriptionChange[clicks];
 }
 
 function prev(){
 clicks --;
 if(clicks <= -1) clicks = 1;
 img.src = pictureChange[clicks];
-desc = description[clicks];
+desc = descriptionChange[clicks];
 }
-
