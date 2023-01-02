d0c726f6f5665b0cb90a558a718183d4e1b68f1e
mcarri355 <carrillomatisa@gmail.com>
2023-01-01 17:34:51 -0700

    timeline images

--- aboutUs.js
+++ aboutUs.js
@@ -1,21 +1,21 @@
 let img = document.getElementById("timelinePic")
 let desc = document.getElementById("timelineDesc").innerHTML
-let pictureChange = ["firstPlace.jpg","ratatouille.jpg"]
+let pictureChange = ["cookbook.jpg","grandOpening.webp","chef.jpg","ratatouille.jpg", "bestOfPhoenix.png", "souffle.jpg", "2michelinStar.png", "michelinStar.png","gusteauDish.jpg"]
 let descriptionChange = ["hello","hi"]
 let clicks = 0;
 
 img.src = pictureChange[clicks]
-desc = descriptionChange[clicks]
+desc.innerHTML = descriptionChange[clicks]
 function next(){
 clicks++;
-if(clicks >= 2) clicks = 0;
+if(clicks >= 9) clicks = 0;
 img.src = pictureChange[clicks];
 desc = descriptionChange[clicks];
 }
 
 function prev(){
 clicks --;
-if(clicks <= -1) clicks = 1;
+if(clicks <= -1) clicks = 8;
 img.src = pictureChange[clicks];
 desc = descriptionChange[clicks];
 }
