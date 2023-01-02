6ea19cc9c0ab0b3b0b213054c641db50e060e5df
mcarri355 <carrillomatisa@gmail.com>
2023-01-02 02:20:20 -0700

    delete timeline

--- aboutUs.js
+++ aboutUs.js
@@ -1,21 +1,3 @@
-let img = document.getElementById("timelinePic")
-let desc = document.getElementById("timelineDesc").innerHTML
-let pictureChange = ["cookbook.jpg","grandOpening.webp","chef.jpg","ratatouille.jpg", "bestOfPhoenix.png", "souffle.jpg", "2michelinStar.png", "michelinStar.png","gusteauDish.jpg"]
-let descriptionChange = ["hello","hi"]
-let clicks = 0;
 
-img.src = pictureChange[clicks]
-desc.innerHTML = descriptionChange[clicks]
-function next(){
-clicks++;
-if(clicks >= 9) clicks = 0;
-img.src = pictureChange[clicks];
-desc = descriptionChange[clicks];
-}
 
-function prev(){
-clicks --;
-if(clicks <= -1) clicks = 8;
-img.src = pictureChange[clicks];
-desc = descriptionChange[clicks];
-}
+
