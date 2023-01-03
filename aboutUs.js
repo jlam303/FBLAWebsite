459f62d7ad6324f2c686257865ef82fb322b8682
mcarri355 <carrillomatisa@gmail.com>
2023-01-03 01:35:08 -0700

    animations

--- aboutUs.js
+++ aboutUs.js
@@ -1,3 +1,45 @@
+//Scroll animation for awards text
+const observer = new IntersectionObserver((entries) => {
+    entries.forEach((entry)=>{
+        console.log(entry)
+        if(entry.isIntersecting){
+            entry.target.classList.add('show');
+        }
+        else{
+            entry.target.classList.remove('show')
+        }
+    })
+})
+const hiddenElements = document.querySelectorAll(".hidden")
+hiddenElements.forEach((el)=> observer.observe(el))
 
 
+//Scroll animation for michelin star image
+const observerStar = new IntersectionObserver((entries) => {
+    entries.forEach((entry)=>{
+        console.log(entry)
+        if(entry.isIntersecting){
+            entry.target.classList.add('showStarPic');
+        }
+        else{
+            entry.target.classList.remove('showStarPic')
+        }
+    })
+})
+const hiddenStar = document.querySelectorAll(".hiddenStarPic")
+hiddenStar.forEach((el)=> observerStar.observe(el))
 
+//Scroll animation for Best of Phoenix image
+const observerBest = new IntersectionObserver((entries) => {
+    entries.forEach((entry)=>{
+        console.log(entry)
+        if(entry.isIntersecting){
+            entry.target.classList.add('showBestPic');
+        }
+        else{
+            entry.target.classList.remove('showBestPic')
+        }
+    })
+})
+const hiddenBest = document.querySelectorAll(".hiddenBestPic")
+hiddenBest.forEach((el)=> observerBest.observe(el))
