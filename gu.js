8caeb4bdb619a202a6c098647843fc3014f2c912
Sarc4sticNerd <jplam904@gmail.com>
2023-01-05 01:23:47 -0700

    Image folder and redesigned slide show
    slide show changed but transitions and hover effects still needed

--- gu.js
+++ gu.js
@@ -1,26 +1,47 @@
-var img = document.getElementById("img");
-let imgPreview1 = document.getElementById("preview1");
-let imgPreview2 = document.getElementById("preview2");
-let imgPreview3 = document.getElementById("preview3");
-var images = ["shrimp.png","escargots.png","beef.png","fish.png","choco.png"], x=0;
-
+var slideInt=1;
+var playing = setInterval(slideShowFor2,2000)
+var hold;
 function slideShowFor(){
-    if(x == images.length-1) x=0;
-    else x++;
-    img.src = images[x];
-    imgPreview2.src = images[x];
-    if(x != 0) imgPreview1.src = images[x-1];
-    else imgPreview1.src = images[images.length-1]
-    if(x != images.length-1) imgPreview3.src = images[x+1];
-    else imgPreview3.src = images[0]
+    slidePause();
+    slideInt++;
+    if(slideInt > 6) slideInt = 1;
+    slideChange();
+}
+function slideShowFor2(){
+    slideInt++;
+    if(slideInt > 6) slideInt = 1;
+    slideChange();
 }
 function slideShowBack(){
-    if(x == 0) x=images.length-1;
-    else x--;
-    img.src = images[x];
-    imgPreview2.src = images[x];
-    if(x != 0) imgPreview1.src = images[x-1];
-    else imgPreview1.src = images[images.length-1]
-    if(x != images.length-1) imgPreview3.src = images[x+1];
-    else imgPreview3.src = images[0]
+    slidePause();
+    slideInt--;
+    if(slideInt < 1) slideInt = 6;
+    slideChange();
+}
+
+var desc = ["Soupe à L'oignon","Boeuf Bourguignon","Steak Tartare","Confit de Canard","Bouillabaisse","Tartiflette"];
+function slideChange(){
+    for(let i = 1;i<=6;i++)
+    {
+        hold = document.getElementById("img" + i)
+        hold.style.visibility = "hidden"
+    }
+    document.getElementById("img" + slideInt).style.visibility = "visible"
+    document.getElementById("slideCaption").innerHTML = desc[slideInt-1]
+}
+var play = true;
+function slidePlay(){
+    if(play == true){
+        slidePause();
+    }
+    else{
+        document.getElementById("pause").src = "./images/icons8-pause-50.png"
+        playing = setInterval(slideShowFor2,2000);
+        play = true
+    }
+}
+function slidePause(){
+    document.getElementById("pause").src = "./images/icons8-play-50.png"
+    clearInterval(playing)
+    play = false;
 }
