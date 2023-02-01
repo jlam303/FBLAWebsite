cb344e569e8364cac23c92275a9070f986356515
Sarc4sticNerd <jplam904@gmail.com>
2023-01-31 21:58:10 -0700

    basic animation test

--- main.js
+++ main.js
@@ -1,15 +1,23 @@
-let keyframes = [
-    {left: "0"},
-    {left: window.innerWidth + "px"},
-    {left: "0"},
-    {right: window.innerWidth + "px"}
-]
-let timing = {
-    duration: 12000,
-    iterations: Infinity,
-    easing: "ease-in-out",
-}
-let mainAnim = document.getElementById("imgRows")
+let mainAnim = document.getElementsByClassName("imgRows")
 function startAnimation(){
-    mainAnim.animate(keyframes,timing)
+    let keyframes = [
+        {left: 0},
+        {left: "-" + window.innerWidth + "px"},
+        {left: 0},
+    ]    
+    let dur = window.innerWidth * 3.5
+    let timing = {
+        duration: dur,
+        iterations: Infinity,
+        easing: "linear",
+    }
+    let timing2 = {
+        delay: "-" + window.innerWidth * 1.75,
+        duration: dur,
+        iterations: Infinity,
+        easing: "linear",
+    }
+    mainAnim[0].animate(keyframes,timing)
+    mainAnim[1].animate(keyframes,timing2)
+    mainAnim[2].animate(keyframes,timing)
 }
