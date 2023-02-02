f9c97a680bc808b524de943d29e5f97cacb86c27
jlam303 <jplam904@gmail.com>
2023-02-01 18:19:23 -0700

    nav hover

--- nav.js
+++ nav.js
@@ -1,13 +1,15 @@
+const navLinks = document.querySelectorAll(".navLinks")
+navLinks.map( link => {
+    link.addEventListener("mouseenter",hoverEffect)
+})
 document.getElementById("navButton").addEventListener("click",navBar)
 let openNav = false;
 function navBar(){
     if(openNav == false){
-        
 
         openNav = true;
     }
     else{
-        
         
         openNav = false;
     }
