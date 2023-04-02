07f9fc3a4d283be0de7d71115c05e0c03322b66a
Sarc4sticNerd <jplam904@gmail.com>
2023-04-02 01:02:14 -0700

    Imported stuff

--- nav.js
+++ nav.js
@@ -1,6 +1,14 @@
+<<<<<<< Updated upstream
 function openM(){
     document.getElementById("navPop").style.display = "flex";
 }
 function closeM(){
     document.getElementById("navPop").style.display = "none";
 }
+=======
+$(function(){
+    $("i").on("click",function(){
+        $(this).slideUp(1000)
+    })
+})
+>>>>>>> Stashed changes
