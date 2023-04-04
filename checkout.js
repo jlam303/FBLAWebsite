2e15dfd0cd743a51ce91d4e5853a4ec924564962
MatthewAndresWestMec <jplam904@gmail.com>
2023-04-03 22:29:19 -0700

    update 5

--- checkout.js
+++ checkout.js
@@ -3,4 +3,6 @@ function reprice(){
 }
 function checkout(){
     document.querySelectorAll("#checkout input").forEach(element => element.value = "")
+    document.querySelector("#totals button").innerHTML = "Thank You For Your Purchase"
+    setTimeout()
 }
